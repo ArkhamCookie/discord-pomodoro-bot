f0bb05623648069c05994d9189ab60f35419087b
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-03-31 21:41:42 -0500

    Feat: Add first button for help menu

--- main.py
+++ main.py
@@ -11,8 +11,13 @@ bot = discord.Bot()
 async def on_ready():
     print(f"{bot.user} is ready and online!")
 
+class HelpView(discord.ui.View):
+    @discord.ui.button(label='<', style=discord.ButtonStyle.primary)
+    async def button_callback(self, button, interaction):
+        await interaction.response.send_message('You clicked the button!')
+
 @bot.slash_command(name = 'help', description = 'Display the help message')
 async def help(ctx):
-    await ctx.respond('Use /help to display this help message')
+    await ctx.respond('Help Menu:', view=HelpView())
 
 bot.run(os.getenv('TOKEN'))
