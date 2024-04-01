f468c2023c5b65c0fee43af2fa17e8349e56e2a4
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-03-31 21:24:24 -0500

    Feat: Convert hello command to start of help command

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ bot = discord.Bot()
 async def on_ready():
     print(f"{bot.user} is ready and online!")
 
-@bot.slash_command(name = 'hello', description = 'Say hello to the bot')
-async def hello(ctx):
-    await ctx.respond('Hey!')
+@bot.slash_command(name = 'help', description = 'Display the help message')
+async def help(ctx):
+    await ctx.respond('Use /help to display this help message')
 
 bot.run(os.getenv('TOKEN'))
