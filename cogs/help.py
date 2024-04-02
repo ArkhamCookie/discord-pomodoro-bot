9c4b7d0f5aeb933ed62c419511a18de52a9ee5ab
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-04-01 21:05:52 -0500

    Feat: Setup Paginator

--- cogs/help.py
+++ cogs/help.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.ext.pages import Paginator, Page
 
 class Help(commands.Cog):
     def __init__(self, bot):
@@ -7,7 +8,16 @@ class Help(commands.Cog):
 
     @discord.slash_command(name = "help", description = "Display the help message")
     async def help(self, ctx):
-        await ctx.respond('Use `/help` to display this message')
+        pages = [
+            Page(embeds=[discord.Embed(title="Help", color=discord.Color.yellow())]),
+            Page(content="Use `/help` to display this message")
+        ]
+        paginator = Paginator(pages=pages, author_check=True, disable_on_timeout=False)
+        paginator.remove_button("first")
+        paginator.remove_button("last")
+
+        # await ctx.respond("Use `/help` to display this message")
+        await paginator.respond(ctx.interaction)
 
 def setup(bot):
     bot.add_cog(Help(bot))
